a2e431ee81bfe381e7f9646a4d447d8cf410c838
ibado <ignaciobado.unc@gmail.com>
2020-09-27 19:48:17 -0300

    Add placeholder for project create

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{App, /*Arg,*/ SubCommand};
+use clap::{App, Arg, SubCommand};
 
 fn main(){
     let matches = App::new("gitlab-cli")
@@ -9,8 +9,34 @@ fn main(){
             SubCommand::with_name("status")
                 .about("print the gitlab status")
         )
+        .subcommand(
+            SubCommand::with_name("project")
+                .about("create or manage existing projects")
+                .subcommand(
+                    SubCommand::with_name("create")
+                        .arg(
+                            Arg::with_name("name")
+                                .long("name")
+                                .index(1)
+                                .required(true)
+                        )
+                )
+        )
         .get_matches();
+
     if matches.is_present("status") {
         println!("status is not yet implemented..");
     }
+    if let Some(m) = matches.subcommand_matches("project") {
+        if let Some(m) = m.subcommand_matches("create") {
+            if m.is_present("name") {
+                let name = m.value_of("name").unwrap();
+                create_project(&name)
+            }
+        }
+    }
+}
+
+fn create_project(name: &str) {
+    println!("creating project with name: {}...", name)
 }
