1e734e03a692a3b74efe4338b31f44d29610bea8
ibado <ignaciobado.unc@gmail.com>
2021-08-21 14:08:40 -0300

    Change Clap builder style for derive macros

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,46 @@
-use clap::{App, Arg, ArgMatches};
-
-const LOGIN: &str = "login";
-const STATUS: &str = "status";
-const PROJECT: &str = "project";
-const CREATE: &str = "create";
-const LIST: &str = "list";
-const NAME: &str = "name";
+use clap::Clap;
 
 fn main() {
-    let matches = get_matches();
-    if matches.is_present(STATUS) {
-        println!("status is not implemented yet..");
-    } else if matches.is_present(LOGIN) {
-        let result = glab::login();
-        if result.is_err() {
-            print!("Error: {}", result.err().unwrap().to_string());
-        }
-    } else if let Some(project) = matches.subcommand_matches(PROJECT) {
-        if let Some(create) = project.subcommand_matches(CREATE) {
-            if create.is_present(NAME) {
-                let name = create.value_of(NAME).unwrap();
-                glab::create_project(&name);
+
+    let opts = Opts::parse();
+
+    match opts.subcommand {
+        SubCommand::Status => println!("status is not implemented yet.."),
+        SubCommand::Login => {
+            let result = glab::login();
+            if result.is_err() {
+                print!("Error: {}", result.err().unwrap().to_string());
             }
-        } else if let Some(_) = project.subcommand_matches(LIST) {
-            glab::list_projects()
-        }
-    }
+        },
+        SubCommand::Project(cmd) => match cmd {
+            ProjectCommand::Create(arg) => glab::create_project(&arg.name),
+            ProjectCommand::List => glab::list_projects(),
+        },
+    };
 }
 
-fn get_matches() -> ArgMatches {
-    let app = App::new("glab")
-        .version("0.1")
-        .author("Ignacio Bado")
-        .about("Allow you to manage your gitlab projects without leaving the terminal")
-        .subcommand(App::new(STATUS)
-            .about("print the gitlab status")
-        )
-        .subcommand(App::new(LOGIN)
-            .about("Interactively set Gitlab credentials")
-        )
-        .subcommand(App::new(PROJECT)
-            .about("create or manage existing projects")
-            .subcommand(App::new(CREATE)
-                .about("Create a new project with the given name")
-                .arg(Arg::new(NAME)
-                    .index(1)
-                    .required(true)
-                )
-            )
-            .subcommand(App::new(LIST)
-                .about("List the existing projects")
-            )
-        );
-    app.get_matches()
+#[derive(Clap)]
+#[clap(version = "0.0.1", author = "Ignacio Bado")]
+struct Opts {
+    #[clap(subcommand)]
+    subcommand: SubCommand,
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    Status,
+    Login,
+    Project(ProjectCommand),
+
+}
+
+#[derive(Clap)]
+enum ProjectCommand {
+    Create(CreateCmd),
+    List,
+}
+
+#[derive(Clap)]
+struct CreateCmd {
+    name: String,
 }
